28ccfde72b7d0124ac50342e83648b0a348fb987
Undead8 <cousineau915@hotmail.com>
2018-06-02 16:02:41 -0400

    Change generator nomenclature

--- generator.rb
+++ generator.rb
@@ -1,14 +1,14 @@
 class RandomGenerator
 
-  def self.random_date
+  def self.date
     rand(Date.today.prev_year(100)..Date.today.prev_year(50))
   end
 
-  def self.random_clients(list_of_names, amount = 1000)
-    Array.new(amount) { Client.new(list_of_names.sample, random_date) }
+  def self.clients(list_of_names, amount = 1000)
+    Array.new(amount) { Client.new(list_of_names.sample, date) }
   end
 
-  def self.random_contracts(list_of_clients)
+  def self.contracts(list_of_clients)
     list_of_clients.map { |client| Contract.new(client, rand(10000..1000000))}
   end
 end
